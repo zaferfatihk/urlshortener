8146d4a9f112c4b48dd77861d89fe904840e4ef0
Zafer Fatih Koyuncu <zaferfatih@gmail.com>
2024-12-14 21:54:28 +0300

    simple user register / login with thymeleaf.

--- src/main/java/com/upwork/urlshortener/rest/UrlController.java
+++ src/main/java/com/upwork/urlshortener/rest/UrlController.java
@@ -1,20 +1,35 @@
 package com.upwork.urlshortener.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
 public class UrlController {
-    
-    @GetMapping("/")
-    public String home() {
-        return "index";
-    }
-    
-    @GetMapping("/secured")
-    public String helloSecured() {
-        return new String("Hello Secured World");
-    }
-    
-} 
+
+    private static final Logger logger = LoggerFactory.getLogger(UrlController.class);
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostMapping("/register")
+    public ModelAndView register(@RequestParam("username") String username, @RequestParam("password") String password) {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles("USER")
+                .build();
+        ((InMemoryUserDetailsManager) userDetailsService).createUser(user);
+
+        return new ModelAndView("login");
+    } 
+}
