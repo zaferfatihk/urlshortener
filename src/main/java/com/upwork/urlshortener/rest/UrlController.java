3d46512b961038ef75334849b5a8ee4a0c2a39de
Zafer Fatih Koyuncu <zaferfatih@gmail.com>
2024-12-15 08:26:43 +0300

    new controller

--- src/main/java/com/upwork/urlshortener/rest/UrlController.java
+++ src/main/java/com/upwork/urlshortener/rest/UrlController.java
@@ -1,10 +1,23 @@
 package com.upwork.urlshortener.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/url")
 public class UrlController {
+    private static final Logger logger = LoggerFactory.getLogger(UrlController.class);
+
+    @PostMapping("/shorten")
+    public String postMethodName(@RequestParam("url") String url) {
+        logger.info("Shortening URL: " + url);        
+        
+        return "shortened";
+    }
     
 }
